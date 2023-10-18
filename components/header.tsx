f1c3e9c76b9e0d39a7f9b2a6524fce213b3d91d1
mfonobongdev <93042878+mfonobongdev@users.noreply.github.com>
2023-10-18 11:34:07 +0100

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -5,14 +5,14 @@ import Button from './button'
 
 export default function Header() {
   return (
-    <header className='fixed top-0 left-0 w-full border-b border-white-a08 backdrop-blur-[12px]'>
+    <header className='fixed left-0 top-0 w-full border-b border-white-a08 backdrop-blur-[12px]'>
       <Container className='flex h-[var(--navigation-height)] items-center'>
         <Link href={'/'}>
           <Logo className='mb-[0.2rem]' />
         </Link>
 
         <nav className='h-full'>
-          <ul className='flex items-center h-full [&_a]:text-sm [&_a:hover]:text-grey [&_a]:transition-colors [&_li]:ml-6'>
+          <ul className='flex h-full items-center [&_a:hover]:text-grey [&_a]:text-sm [&_a]:transition-colors [&_li]:ml-6'>
             <li>
               <Link href={'#'}>Features</Link>
             </li>
@@ -34,8 +34,8 @@ export default function Header() {
           </ul>
         </nav>
 
-        <div className='ml-auto h-full flex items-center'>
-          <Link className='text-sm mr-6' href={'#'}>
+        <div className='ml-auto flex h-full items-center'>
+          <Link className='mr-6 text-sm' href={'#'}>
             Log In
           </Link>
           <Button href='#'>Sign Up</Button>
